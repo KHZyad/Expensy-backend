57f3c061577434c5715dd17118e76335c64fcf02
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-02-05 22:14:18 +0100

    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from flask_cors import CORS
-from app.routes.transactions import transactions_bp
-from app.routes.reminders import reminders_bp
-from app.routes.goals import goals_bp
-from app.routes.notifications import notifications_bp
-from app.routes.users import users_bp
+from app.routes import users, transactions, reminders, goals, notifications
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
 
-    app.register_blueprint(transactions_bp, url_prefix="/transactions")
-    app.register_blueprint(reminders_bp, url_prefix="/reminders")
-    app.register_blueprint(goals_bp, url_prefix="/goals")
-    app.register_blueprint(notifications_bp, url_prefix="/notifications")
-    app.register_blueprint(users_bp, url_prefix="/users")
+    app.register_blueprint(users.users_bp, url_prefix="/api/users")
+    app.register_blueprint(transactions.transactions_bp, url_prefix="/api/transactions")
+    app.register_blueprint(reminders.reminders_bp, url_prefix="/api/reminders")
+    app.register_blueprint(goals.goals_bp, url_prefix="/api/goals")
+    app.register_blueprint(notifications.notifications_bp, url_prefix="/api/notifications")
+
+    @app.route("/")
+    def index():
+        return "Expenssy Backend is Running!"
 
     return app
