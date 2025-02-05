3d11774adb7bfb37542af813d5830b8de0f2107c
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-02-05 21:55:47 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-def handler(event, context):
-    return app(event, context)  # Required for Vercel
+if __name__ != "__main__":
+    application = app  
