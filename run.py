e9d79a48efa66b731429105a9db60cc55cbc4a47
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-02-05 21:24:51 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app()
+app = create_app()  
 
-def handler(event, context):
-    return app(event, context)  # Vercel-compatible
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
